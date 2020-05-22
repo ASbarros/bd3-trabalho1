541f1f5c73a07cb17105d30b5f74e249de6cb794
André Teixeira <40339686+Andre-Luiz-Teixeira@users.noreply.github.com>
2020-05-21 22:09:44 -0300

    jpa dao Vendedor Comissao

--- src/DAO/DaoVendedorComissao.java
+++ src/DAO/DaoVendedorComissao.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import model.MdlVendedorComissao;
 
 public class DaoVendedorComissao {
@@ -24,59 +25,31 @@ public class DaoVendedorComissao {
     }
 
     public void inserir(MdlVendedorComissao comissao) {
-        PreparedStatement comandoSQL;
-
-        String sql = "insert into vendedor_comissao (percentual_vendcom, vlrcomissao_vendcom, id_vend_vendcom, id_ped_vendcom) values (?, ?, ?, ?)";
-
         try {
-            comandoSQL = minhaConexao.prepareStatement(sql);
-
-            comandoSQL.setDouble(1, comissao.getPercentual());
-            comandoSQL.setDouble(2, comissao.getValor());
-            comandoSQL.setInt(3, comissao.getVendedor().getId());
-            comandoSQL.setInt(4, comissao.getPedido().getId());
-
-            comandoSQL.executeUpdate();
-            comandoSQL.close();
-        } catch (SQLException e) {
+            minhaConexao.persist(comissao);
+            minhaConexao.getTransaction().commit();
+        } catch (Exception e) {
+            minhaConexao.getTransaction().rollback();
             System.err.println("Erro ao incluir pedido vendedor comissao: " + e.getMessage());
         }
     }
 
     public void atualizar(MdlVendedorComissao comissao) {
-        PreparedStatement comandoSQL;
-
-        String sql = "update vendedor_comissao set percentual_vendcom = ?, vlrcomissao_vendcom = ?, id_vend_vendcom = ?, id_ped_vendcom = ? where id_vendcom = ?";
-
         try {
-            comandoSQL = minhaConexao.prepareStatement(sql);
-
-            comandoSQL.setDouble(1, comissao.getPercentual());
-            comandoSQL.setDouble(2, comissao.getValor());
-            comandoSQL.setInt(3, comissao.getVendedor().getId());
-            comandoSQL.setInt(4, comissao.getPedido().getId());
-            comandoSQL.setInt(5, comissao.getId());
-
-            comandoSQL.executeUpdate();
-            comandoSQL.close();
-        } catch (SQLException e) {
+            minhaConexao.persist(comissao);
+            minhaConexao.getTransaction().commit();
+        } catch (Exception e) {
+            minhaConexao.getTransaction().rollback();
             System.err.println("Erro ao atualizar vendedor comissao: " + e.getMessage());
         }
     }
 
     public void excluir(int pk) {
-        PreparedStatement comandoSQL;
-
-        String sql = "delete from vendedor_comissao where id_vendcom = ?";
-
         try {
-            comandoSQL = minhaConexao.prepareStatement(sql);
-
-            comandoSQL.setInt(1, pk);
-
-            comandoSQL.executeUpdate();
-            comandoSQL.close();
-        } catch (SQLException e) {
+            minhaConexao.remove(pk);
+            minhaConexao.getTransaction().commit();
+        } catch (Exception e) {
+            minhaConexao.getTransaction().rollback();
             System.err.println("Erro ao deletar vendedor comissao: " + e.getMessage());
         }
     }
@@ -86,25 +59,9 @@ public class DaoVendedorComissao {
     }
 
     public MdlVendedorComissao recuperar(int index) {
-        String sql = "select id_vendcom, percentual_vendcom, vlrcomissao_vendcom, id_vend_vendcom, id_ped_vendcom from vendedor_comissao where id_vendcom = ?";
-
         try {
-            PreparedStatement stp = minhaConexao.prepareStatement(sql);
-            stp.setInt(1, index);
-            ResultSet resultado = stp.executeQuery();
-
-            MdlVendedorComissao obj = new MdlVendedorComissao();
-
-            if (resultado.next()) {
-                obj.setId(Integer.parseInt(resultado.getString("id_vendcom")));
-                obj.setPercentual(resultado.getDouble("percentual_vendcom"));
-                obj.setValor(resultado.getDouble("vlrcomissao_vendcom"));
-                obj.setVendedor(new DaoVendedor().recuperar(resultado.getInt("id_vend_vendcom")));
-                obj.setPedido(new DaoPedido().recuperar(resultado.getInt("id_ped_vendcom")));
-            }
-            return obj;
-
-        } catch (SQLException ex) {
+            return minhaConexao.find(MdlVendedorComissao.class, index);
+        } catch (Exception ex) {
             Logger.getLogger(MdlVendedorComissao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
@@ -112,25 +69,19 @@ public class DaoVendedorComissao {
 
     public ArrayList<MdlVendedorComissao> recuperarTodos() {
         String sql = "select id_vendcom, percentual_vendcom, vlrcomissao_vendcom, id_vend_vendcom, id_ped_vendcom from vendedor_comissao";
-        ArrayList<MdlVendedorComissao> lista = new ArrayList();
+        return recuperar(sql);
+    }
+    
+    public ArrayList<MdlVendedorComissao> recuperar(String SQL) {
+        ArrayList<MdlVendedorComissao> ListaCliente = new ArrayList<>();
         try {
-            PreparedStatement stp = minhaConexao.prepareStatement(sql);
-            ResultSet resultado = stp.executeQuery();
-
-            if (resultado.next()) {
-                MdlVendedorComissao obj = new MdlVendedorComissao();
-                obj.setId(Integer.parseInt(resultado.getString("id_vendcom")));
-                obj.setPercentual(resultado.getDouble("percentual_vendcom"));
-                obj.setValor(resultado.getDouble("vlrcomissao_vendcom"));
-                obj.setVendedor(new DaoVendedor().recuperar(resultado.getInt("id_vend_vendcom")));
-                obj.setPedido(new DaoPedido().recuperar(resultado.getInt("id_ped_vendcom")));
-                lista.add(obj);
-            }
-            return lista;
-
-        } catch (SQLException ex) {
-            Logger.getLogger(MdlVendedorComissao.class.getName()).log(Level.SEVERE, null, ex);
+            Query minhaQuery = minhaConexao.createQuery(SQL);
+            ListaCliente = (ArrayList<MdlVendedorComissao>) minhaQuery.getResultList();
+        } catch (Exception e) {
+            System.out.println("Erro ao recuperar conta: " + e);
+            minhaConexao.getTransaction().rollback();
         }
-        return null;
+        minhaConexao.getTransaction().commit();
+        return ListaCliente;
     }
 }
