f8d20800c04cd9abb38ad2aabb634cd2455203a8
André Teixeira <40339686+Andre-Luiz-Teixeira@users.noreply.github.com>
2020-05-21 22:00:49 -0300

    jpa dao produto

--- src/DAO/DaoProduto.java
+++ src/DAO/DaoProduto.java
@@ -7,73 +7,49 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import model.MdlCliente;
 import model.MdlProduto;
 
 public class DaoProduto {
 
-    private Connection minhaConexao;
+    private EntityManager minhaConexao;
 
     public DaoProduto() {
         minhaConexao = FabricaConexao.getConexaoPADRAO();
     }
 
-    public DaoProduto(Connection cnx) {
+    public DaoProduto(EntityManager cnx) {
         minhaConexao = cnx;
     }
 
     public void inserir(MdlProduto produto) {
-        PreparedStatement comandoSQL;
-
-        String sql = "insert into produto (descricao_prod, saldo_prod, unidade_prod, valor_prod) values (?,?,?,?);";
         try {
-            comandoSQL = minhaConexao.prepareStatement(sql);
-
-            comandoSQL.setString(1, produto.getDescricao());
-            comandoSQL.setDouble(2, produto.getSaldo());
-            comandoSQL.setString(3, produto.getUnidade());
-            comandoSQL.setDouble(4, produto.getValor());
-
-            comandoSQL.executeUpdate();
-            comandoSQL.close();
-        } catch (SQLException e) {
+            minhaConexao.persist(produto);
+            minhaConexao.getTransaction().commit();
+        } catch (Exception e) {
+            minhaConexao.getTransaction().rollback();
             System.err.println("Erro ao incluir produto: " + e.getMessage());
         }
     }
 
     public void atualizar(MdlProduto produto) {
-        PreparedStatement comandoSQL;
-
-        String sql = "update produto set descricao_prod = ?, saldo_prod = ?, unidade_prod = ?, valor_prod = ? where id_prod = ?;";
-
         try {
-            comandoSQL = minhaConexao.prepareStatement(sql);
-
-            comandoSQL.setString(1, produto.getDescricao());
-            comandoSQL.setDouble(2, produto.getSaldo());
-            comandoSQL.setString(3, produto.getUnidade());
-            comandoSQL.setDouble(4, produto.getValor());
-            comandoSQL.setInt(5, produto.getId());
-
-            comandoSQL.executeUpdate();
-            comandoSQL.close();
-        } catch (SQLException e) {
+            minhaConexao.persist(produto);
+            minhaConexao.getTransaction().commit();
+        } catch (Exception e) {
+            minhaConexao.getTransaction().rollback();
             System.err.println("Erro ao atualizar produto: " + e.getMessage());
         }
     }
 
     public void excluir(int pk) {
-        PreparedStatement comandoSQL;
-
-        String sql = "delete from produto where id_prod = ?";
-
         try {
-            comandoSQL = minhaConexao.prepareStatement(sql);
-
-            comandoSQL.setInt(1, pk);
-
-            comandoSQL.executeUpdate();
-            comandoSQL.close();
-        } catch (SQLException e) {
+            minhaConexao.remove(pk);
+            minhaConexao.getTransaction().commit();
+        } catch (Exception e) {
+            minhaConexao.getTransaction().rollback();
             System.err.println("Erro ao deletar produto: " + e.getMessage());
         }
     }
@@ -83,52 +59,29 @@ public class DaoProduto {
     }
 
     public MdlProduto recuperar(int index) {
-        String sql = "select id_prod, descricao_prod, saldo_prod, unidade_prod, valor_prod from produto where id_prod = ?";
-
         try {
-            PreparedStatement stp = minhaConexao.prepareStatement(sql);
-            stp.setInt(1, index);
-            ResultSet resultado = stp.executeQuery();
-
-            MdlProduto obj = new MdlProduto();
-
-            if (resultado.next()) {
-                obj.setId(Integer.parseInt(resultado.getString("id_prod")));
-                obj.setDescricao(resultado.getString("descricao_prod"));
-                obj.setSaldo(resultado.getDouble("saldo_prod"));
-                obj.setUnidade(resultado.getString("unidade_prod"));
-                obj.setValor(Double.parseDouble(resultado.getString("valor_prod")));
-
-            }
-            return obj;
-
-        } catch (SQLException ex) {
-            Logger.getLogger(MdlProduto.class.getName()).log(Level.SEVERE, null, ex);
+            minhaConexao.find(MdlProduto.class, index);
+        } catch (Exception e) {
+            Logger.getLogger(MdlProduto.class.getName()).log(Level.SEVERE, null, e);
         }
         return null;
     }
 
     public ArrayList<MdlProduto> recuperarTodos() {
         String sql = "select id_prod, descricao_prod, saldo_prod, unidade_prod, valor_prod from produto";
-        ArrayList<MdlProduto> lista = new ArrayList();
+        return recuperar(sql);
+    }
+    
+    public ArrayList<MdlProduto> recuperar(String SQL) {
+        ArrayList<MdlProduto> ListaCliente = new ArrayList<>();
         try {
-            PreparedStatement stp = minhaConexao.prepareStatement(sql);
-            ResultSet resultado = stp.executeQuery();
-
-            while (resultado.next()) {
-                MdlProduto obj = new MdlProduto();
-                obj.setId(Integer.parseInt(resultado.getString("id_prod")));
-                obj.setDescricao(resultado.getString("descricao_prod"));
-                obj.setSaldo(resultado.getDouble("saldo_prod"));
-                obj.setUnidade(resultado.getString("unidade_prod"));
-                obj.setValor(resultado.getDouble("valor_prod"));
-                lista.add(obj);
-            }
-            return lista;
-
-        } catch (SQLException ex) {
-            Logger.getLogger(MdlProduto.class.getName()).log(Level.SEVERE, null, ex);
+            Query minhaQuery = minhaConexao.createQuery(SQL);
+            ListaCliente = (ArrayList<MdlProduto>) minhaQuery.getResultList();
+        } catch (Exception e) {
+            System.out.println("Erro ao recuperar produto: " + e);
+            minhaConexao.getTransaction().rollback();
         }
-        return null;
+        minhaConexao.getTransaction().commit();
+        return ListaCliente;
     }
 }
